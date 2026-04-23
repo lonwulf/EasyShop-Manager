0f8fecc00eafa586f605b28e28dadb7e216c63ec
Lonwulf <kenkiwa.kw@gmail.com>
2026-04-23 14:14:34 +0300

    add missing di

--- app/src/main/java/com/lonwulf/labs/easyshopmanager/di/AppModule.kt
+++ app/src/main/java/com/lonwulf/labs/easyshopmanager/di/AppModule.kt
@@ -1,13 +1,22 @@
 package com.lonwulf.labs.easyshopmanager.di
 
+import app.cash.sqldelight.db.SqlDriver
 import com.lonwulf.labs.easyshopmanager.data.repository.DatastoreRepositoryImpl
+import com.lonwulf.labs.easyshopmanager.data.repository.SQLRepositoryImpl
+import com.lonwulf.labs.easyshopmanager.data.source.db.DatabaseDriverFactory
+import com.lonwulf.labs.easyshopmanager.db.Catalogue
 import com.lonwulf.labs.easyshopmanager.domain.repository.IDatastoreRepository
+import com.lonwulf.labs.easyshopmanager.domain.repository.ISQLRepository
 import com.lonwulf.labs.easyshopmanager.ui.viewmodel.MainViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
-    viewModel { MainViewModel() }
+    single<SqlDriver> { DatabaseDriverFactory(androidContext()).createDriver() }
+    single<Catalogue> { Catalogue(get()) }
     single<IDatastoreRepository> { DatastoreRepositoryImpl(androidContext()) }
+    single<ISQLRepository> { SQLRepositoryImpl(get()) }
+    viewModel { MainViewModel() }
+
 }
